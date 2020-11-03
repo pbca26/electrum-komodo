7b3372eb8f2ecaad23389a4e9a0b386b68134b3c
pbca26 <pbca26@gmail.com>
2020-11-03 15:38:23 +0300

    update fetch-nspv-bins.sh urls

--- fetch-nspv-bins.sh
+++ fetch-nspv-bins.sh
@@ -3,14 +3,14 @@ mkdir nspv/win64
 mkdir nspv/linux64
 mkdir nspv/osx
 cd nspv/win64
-wget https://github.com/pbca26/libnspv/releases/download/v0.3/nspv-win.tar
+wget https://github.com/pbca26/libnspv/releases/download/v0.3.1/nspv-win.tar
 tar -xvf nspv-win.tar
 rm nspv-win.tar
 cd ../linux64
-wget https://github.com/pbca26/libnspv/releases/download/v0.3/nspv-linux.tar
+wget https://github.com/pbca26/libnspv/releases/download/v0.3.1/nspv-linux.tar
 tar -xvf nspv-linux.tar
 rm nspv-linux.tar
 cd ../osx
-wget https://github.com/pbca26/libnspv/releases/download/v0.3/nspv-osx.tar
+wget https://github.com/pbca26/libnspv/releases/download/v0.3.1/nspv-osx.tar
 tar -xvf nspv-osx.tar
 rm nspv-osx.tar
